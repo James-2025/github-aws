857125855f41ea7f12c495df86d453834ed1e7c2
James-2025 <4al20cs052@gmail.com>
2024-10-30 15:30:34 +0530

    c21

--- main.tf
+++ main.tf
@@ -20,6 +20,24 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_security_group" "ssh_access" {
+  name        = "allow_ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Consider restricting this to GitHub Actions IP ranges for better security
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
