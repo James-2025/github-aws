564c4bfefcaa77ed068d2ce07e52592557abfece
James-2025 <4al20cs052@gmail.com>
2024-10-30 13:20:56 +0530

    c9

--- main.tf
+++ main.tf
@@ -29,6 +29,24 @@ resource "aws_instance" "example" {
     Name = var.name
   }
 
+
+  provisioner "file" 
+  {
+    source      = "index.html"
+    destination = "/var/www/html/index.html"
+  }
+
+  provisioner "remote-exec" 
+  {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install -y apache2",
+      "sudo mv /var/www/html/index.html /var/www/html/",
+      "sudo systemctl start apache2",
+      "sudo systemctl enable apache2"
+    ]
+  }
+
   user_data = <<-EOF
               #!/bin/bash
               sudo yum -y update
